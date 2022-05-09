c7a6b01d431e5a9d798a5b3c7a12f8dd11d789b0
durneja <kinshuk89@gmail.com>
2022-05-09 12:23:51 +0530

    Handling decimals, access checks, updating timestamp, fetch method for all tokens

--- contracts/Feedio/src/FeedioContract.cs
+++ contracts/Feedio/src/FeedioContract.cs
@@ -14,6 +14,8 @@ namespace Feedio
     [ManifestExtra("Author", "durneja")]
     [ManifestExtra("Email", "kinshuk.kar@gmail.com")]
     [ManifestExtra("Description", "Primary contract for Feedio solution with aggregator interfaces and periodic data updates")]
+    [ContractPermission("0x8b6a955ef8026cecaf9393f1734bffe508ce42be", "*")]
+    [ContractTrust("0x8b6a955ef8026cecaf9393f1734bffe508ce42be")]
     public class FeedioContract : SmartContract
     {
 
@@ -23,6 +25,12 @@ namespace Feedio
         protected const byte Prefix_Config = 0x01;
         protected const string Prefix_Config_Owner = "o";
         protected const string Prefix_Config_Updater = "u";
+        protected const string Prefix_Config_Update_Timestamp = "t";
+
+        public delegate void OnPriceUpdatedDelegate();
+        [DisplayName("PricesSubscribed")]
+        public static event OnPriceUpdatedDelegate OnPriceUpdated;
+
 
         private static Boolean VerifyOwner()
         {
@@ -55,6 +63,13 @@ namespace Feedio
 
             configData.Put(Prefix_Config_Owner, (UInt160) ToScripthash("Nc2JPKy62qCWWWSB6Ud6KL275u8yhWGTj5"));
             configData.Put(Prefix_Config_Updater, (UInt160) ToScripthash("NRurDcircwHDFb2aBuiP2QNPHUqa8evAja"));
+            configData.Put((ByteString)"D*" + (ByteString)"BTC", (BigInteger) 4); //9
+            configData.Put((ByteString)"D*" + (ByteString)"ETH", (BigInteger) 4); //8
+            configData.Put((ByteString)"D*" + (ByteString)"NEO", (BigInteger) 6); //8
+            configData.Put((ByteString)"D*" + (ByteString)"GAS", (BigInteger) 6); //9
+            configData.Put((ByteString)"D*" + (ByteString)"BNB", (BigInteger) 6); //9
+            configData.Put((ByteString)"D*" + (ByteString)"MATIC", (BigInteger) 6); //9
+
         }
         public static void UpdateTokenPrice(List<ByteString> tokens, List<BigInteger> prices) {
 
@@ -75,14 +90,53 @@ namespace Feedio
                 i = i + 1;
             }
 
+            StorageMap configData = new StorageMap(Storage.CurrentContext, Prefix_Config);
+            configData.Put(Prefix_Config_Update_Timestamp, (BigInteger)Runtime.Time);
+
+            OnPriceUpdated();
             return;
+        }
+
+        public static BigInteger GetLastUpdatedTime() {
+            StorageMap configData = new StorageMap(Storage.CurrentContext, Prefix_Config);
+            BigInteger lastUpdatedTime = (BigInteger)configData.Get(Prefix_Config_Update_Timestamp);
+
+            return lastUpdatedTime;
+        }
+
+        public static ByteString GetLatestTokenPrices() {
+
+            bool hasValidAccessSender = (bool)Contract.Call(ToScripthash("NdFybtEngSHyfRbUCw1vCe9HZbBtpaX741"), "accessPresentAndValid", CallFlags.ReadOnly, Runtime.CallingScriptHash);
+            bool hasValidAccessCaller = (bool)Contract.Call(ToScripthash("NdFybtEngSHyfRbUCw1vCe9HZbBtpaX741"), "accessPresentAndValid", CallFlags.ReadOnly, Tx.Sender);
+
+            if (!hasValidAccessSender && !hasValidAccessCaller) throw new Exception("Account does not have required access to retrieve prices");
+
+            var iterator = Storage.Find(Storage.CurrentContext, (ByteString) "V1", FindOptions.KeysOnly | FindOptions.RemovePrefix);
+            List<TokenPriceResponse> prices = new List<TokenPriceResponse>();
+            
+            while (iterator.Next()) {
+                
+                BigInteger assetPrice = (BigInteger) Storage.Get(Storage.CurrentContext, (ByteString)"V1" + (ByteString)iterator.Value);
+                StorageMap configData = new StorageMap(Storage.CurrentContext, Prefix_Config);
+
+                TokenPriceResponse priceResponse = new TokenPriceResponse((ByteString) iterator.Value, assetPrice, (BigInteger) configData.Get((ByteString)"D*" + (ByteString)iterator.Value));
+                prices.Add(priceResponse);
+            }
+
+            return StdLib.JsonSerialize(prices);
         }
 
         public static ByteString GetLatestTokenPrice(ByteString asset) {
 
-            BigInteger assetPrice = (BigInteger) Storage.Get(Storage.CurrentContext, (ByteString)"V1" + asset);
+            bool hasValidAccessSender = (bool)Contract.Call(ToScripthash("NdFybtEngSHyfRbUCw1vCe9HZbBtpaX741"), "accessPresentAndValid", CallFlags.ReadOnly, Runtime.CallingScriptHash);
+            bool hasValidAccessCaller = (bool)Contract.Call(ToScripthash("NdFybtEngSHyfRbUCw1vCe9HZbBtpaX741"), "accessPresentAndValid", CallFlags.ReadOnly, Tx.Sender);
 
-            TokenPriceResponse response = new TokenPriceResponse(asset,assetPrice);
+            if (!hasValidAccessSender && !hasValidAccessCaller) throw new Exception("Account does not have required access to retrieve prices");
+
+            BigInteger assetPrice = (BigInteger) Storage.Get(Storage.CurrentContext, (ByteString)"V1" + asset);
+            StorageMap configData = new StorageMap(Storage.CurrentContext, Prefix_Config);
+
+            TokenPriceResponse response = new TokenPriceResponse(asset, assetPrice, (BigInteger) configData.Get((ByteString)"D*" + asset));
             return StdLib.JsonSerialize(response);
         }
 
@@ -115,11 +169,10 @@ namespace Feedio
         public BigInteger value; //Value of the token
         public BigInteger decimals; //Decimals
 
-        public TokenPriceResponse(string tokenName, BigInteger tokenValue) {
+        public TokenPriceResponse(string tokenName, BigInteger tokenValue, BigInteger decimals) {
             this.name = tokenName;
             this.value = tokenValue;
-            this.decimals = 4;
+            this.decimals = decimals;
         }
     }
-
 }
